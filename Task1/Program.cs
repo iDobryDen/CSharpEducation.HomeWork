1e5f739fa7cd81abac901693d23236cba59912ec
Denis Perevoshchikov <DobryDen>
2024-02-07 06:49:44 +0400

    Task1 done!

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,7 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] Nums = new int[5];
+        for ( int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Введите число с индексом : " + i);
+            Nums[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Вы ввели следующие числа: ");
+        foreach(int num in Nums)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
 
